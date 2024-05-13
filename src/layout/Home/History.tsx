60c063ead28ec1b5f9942820bb2c4b4da3195abb
Nawrasse Dahman <nawrassedhm@techie.com>
2024-05-13 15:20:43 +0100

    Update History.tsx

--- src/layout/Home/History.tsx
+++ src/layout/Home/History.tsx
@@ -11,6 +11,10 @@ export const History = () => (
     fontWeight="normal"
     color="textProminent"
   >
+    Nawrasse Dahman was only <Note name="Ten Years Old" words="Ten Years Old" /> when he first watched <Tip name="Ruby Sparks"> "Ruby Sparks."</Tip> By the time the credits rolled, he had already made up his mind that he wanted to be a writer.
+He was convinced that if he could write a book, the main character would come to life and become his closest companion.
+Unfortunately, reality did not align with his expectations, so he turned to writing horror instead.
+This led to the release of his debut novella, "Strangers In The Night." I guess you could say his interpretation of “Friendship” was a little unconventional.
     I Started My Coding <Note name="Journey" words="Journey" /> Back In 2012, When I Saw An Ad For<Tip name="Khanacademy"> Khan Academy</Tip> While Browsing The Web For Games To Play, I Instantly Got Intrigued By It, And Wondered Off In My Mind Picturing Myself As A Really
     <Tip name="Cool"> Cool Hacker</Tip> Like Those In Movies, And I Actually Liked What I Saw, So Here We Are Now After <Note name="10 Years Of Coding" words="10 Years Of Coding" /> And With One <Note name="Simple Mission" words="Simple Mission" /> Is To Make Coding Easier And Accessible For Everybody To Use.
     <br />

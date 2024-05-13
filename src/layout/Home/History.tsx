ad107fb1700535f5980be8ecae6ae02442afc166
Nawrasse Dahman <nawrassedhm@techie.com>
2024-05-13 17:13:28 +0100

    Update History.tsx

--- src/layout/Home/History.tsx
+++ src/layout/Home/History.tsx
@@ -13,8 +13,8 @@ export const History = () => (
 Nawrasse Dahman was only <Note name="Ten Years Old" words="Ten Years Old" /> when he first watched <Tip name="Ruby Sparks"> "Ruby Sparks."</Tip> By the time the credits rolled, he had already made up his mind that he wanted to be a <Note name="Writer" words="Writer" />.
 He was convinced that if he could write a book, the <Tip name="Main Character"> Main Character</Tip> would come to life and become his closest companion.
 <br />
-<br />    
-Unfortunately, <Note name="Reality Did Not Align With His Expectations" words="Reality Did Not Align With His Expectations" />, so he needed to get a degree in something useful and that how his journey as a software developer began.
-This led to the release of his debut novella, <Tip name="Strangers In The Night"> "Strangers In The Night."</Tip> I guess you could say his interpretation of “Friendship” was a little unconventional.
+Unfortunately, <Note name="Reality Did Not Align With His Expectations" words="Reality Did Not Align With His Expectations" />, so he decided to pursue a more practical route in <Tip name="Software Development"> Software Development.</Tip>
+<br />
+Despite this change in direction, his passion for writing remained strong and eventually led to the release of his debut novella, <Tip name="Strangers In The Night"> "Strangers In The Night."</Tip>
   </Heading>
 )
